b77358b5019fab17c3cf273f2fc53f78b39845b4
Danis96 <danis.preldzic@gmail.com>
2019-11-07 22:39:12 +0100

    Updates

---
+++ precache-manifest.9f127ba2c44db48ba8b0699b92ff8eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a7f7b220f26ec212ccd6f202f3a556",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "af2fe9e9df19e9dd9b43",
+    "url": "/contactManager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a7dc79f85b601fdf96b3",
+    "url": "/contactManager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "af2fe9e9df19e9dd9b43",
+    "url": "/contactManager/static/js/2.69ac52c7.chunk.js"
+  },
+  {
+    "revision": "a7dc79f85b601fdf96b3",
+    "url": "/contactManager/static/js/main.d3d7a072.chunk.js"
+  },
+  {
+    "revision": "5f952e25a6251b3ab06f",
+    "url": "/contactManager/static/js/runtime-main.c27e6679.js"
+  }
+]);
